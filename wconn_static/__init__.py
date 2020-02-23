9c3bb795911e261f27d7969551a6c184530216ab
fpemud <fpemud@sina.com>
2020-02-23 23:20:21 +0800

    fix

--- wconn_static/__init__.py
+++ wconn_static/__init__.py
@@ -2,81 +2,43 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import logging
-import pyroute2
-import ipaddress
 
 
 class _PluginObject:
 
-    def init2(self, cfg, tmpDir, ownResolvConf, upCallback, downCallback):
+    def init2(self, cfg, api):
         self.cfg = cfg
-        self.tmpDir = tmpDir
-        self.ownResolvConf = ownResolvConf
-        self.upCallback = upCallback
-        self.downCallback = downCallback
+        self.api = api
         self.logger = logging.getLogger(self.__module__ + "." + self.__class__.__name__)
-        self.businessAttrDict = dict()
         self.bAlive = False
 
     def get_interface(self):
         return "eth0"
 
     def start(self):
-        curCfg = self.cfg["main"]
-        if "nameservers" in curCfg:
-            with open(self.ownResolvConf, "w") as f:
-                for ns in curCfg["nameservers"]:
-                    f.write("nameserver %s\n" % (ns))
-            self.logger.info("Nameservers are \"%s\"." % ("\",\"".join(curCfg["nameservers"])))
         self.logger.info("Started.")
 
     def stop(self):
-        with pyroute2.IPRoute() as ipp:
-            idx = None
-            idx = ipp.link_lookup(ifname="eth0")[0]
-            ipp.link("set", index=idx, state="down")
-            ipp.flush_addr(index=idx)
-            self.downCallback()
-
-        with open(self.ownResolvConf, "w") as f:
-            f.write("")
+        if self.bAlive:
+            self.bAlive = False
+            self.api.deactivate_interface("eth0")
         self.logger.info("Stopped.")
-
-    def is_connected(self):
-        return self.bAlive
-
-    def get_ip(self):
-        assert self.is_connected()
-        curCfg = self.cfg["main"]
-        return curCfg["internet-ip"].split("/")[0]
-
-    def get_netmask(self):
-        assert self.is_connected()
-        curCfg = self.cfg["main"]
-        return curCfg["prefix"].split("/")[1]
-
-    def get_extra_prefix_list(self):
-        assert self.is_connected()
-        return []
-
-    def get_business_attributes(self):
-        assert self.is_connected()
-        return self.businessAttrDict
 
     def interface_appear(self, ifname):
         curCfg = self.cfg["main"]
         if ifname == "eth0":
-            ip = curCfg["prefix"].split("/")[0]
-            bnet = ipaddress.IPv4Network(curCfg["prefix"], strict=False)
-            with pyroute2.IPRoute() as ipp:
-                idx = ipp.link_lookup(ifname="eth0")[0]
-                ipp.link("set", index=idx, state="up")
-                ipp.addr("add", index=idx, address=ip, mask=bnet.prefixlen, broadcast=str(bnet.broadcast_address))
-                if "gateway" in curCfg:
-                    ipp.route('add', dst="0.0.0.0/0", gateway=curCfg["gateway"], oif=idx)
-                if "routes" in curCfg:
-                    for rt in curCfg["routes"]:
-                        ipp.route('add', dst=rt["prefix"], gateway=rt["gateway"], oif=idx)
+            ret = {
+                "prefix": curCfg["prefix"],
+            }
+            if "gateway" in curCfg:
+                ret["gateway"] = curCfg["gateways"]
+            if "nameservers" in curCfg:
+                ret["nameservers"] = curCfg["nameservers"]
+            if "routes" in curCfg:
+                ret["routes"] = curCfg["routes"]
+            if "internet-ip" in curCfg:
+                ret["internet-ip"] = curCfg["internet-ip"]
+            self.api.activate_interface(ifname, ret)
             self.logger.info("Interface \"%s\" managed." % (ifname))
             self.bAlive = True
             return True
@@ -87,5 +49,4 @@ class _PluginObject:
         if ifname == "eth0":
             assert self.bAlive
             self.bAlive = False
-            self.businessAttrDict = dict()
-            self.downCallback()
+            self.api.deactivate_interface("eth0")
